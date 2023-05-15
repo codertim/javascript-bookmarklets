04d6e59c3262c987d12c2c24744c57b9b26f6f6c
Tim F <twforce@yahoo.com>
2023-05-15 16:06:17 -0700

    formatted coded, added alert when everything is fine

--- NoDuplicateIds/no_duplicate_ids.js
+++ NoDuplicateIds/no_duplicate_ids.js
@@ -4,7 +4,16 @@ javascript:
 var idElements = document.querySelectorAll("[id]");
 var elIdNames = [];
 var duplicates = [];
-for (var i=0; i < idElements.length; i++) { if (elIdNames.includes(idElements[i].id)) { duplicates.push(idElements[i].id); } else { elIdNames.push(idElements[i].id); } }
-if (duplicates && duplicates.length >= 1) { var duplicatesStr = duplicates.join(","); alert("Duplicates IDs: " + duplicates); }
+for (var i=0; i < idElements.length; i++) {
+    if (elIdNames.includes(idElements[i].id)) { duplicates.push(idElements[i].id); } 
+    else { elIdNames.push(idElements[i].id); }
+}
+console.log("##### elIdNames:", elIdNames);
+if (duplicates && duplicates.length >= 1) {
+    var duplicatesStr = duplicates.join(",");
+    alert("Duplicates IDs: " + duplicates);
+} else {
+    alert("OK: no duplicates");
+}
 
 
